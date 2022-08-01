360fa86c9578417cc79e5f1c0cee5ec0c3549769
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-01 15:17:45 +0500

    done

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -3,5 +3,11 @@ package lang.print.gaps.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
+
+        int a, b, c;
+        a = 10;
+        b = a;
+        c = b;
+        System.out.println(c);
     }
 }
--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -3,5 +3,18 @@ package lang.print.gaps.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
+        int first = 1;
+        System.out.println(first);
+        int second = 10;
+        System.out.println(second);
+        int third = 100;
+        System.out.println(third);
+        first = 15;
+        System.out.println(first);
+        second = 6;
+        System.out.println(second);
+        third = 4;
+        System.out.println(third);
+
     }
 }
