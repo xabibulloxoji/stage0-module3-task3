23fc017959644d26a661cdfb6125b86329826dbf
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-03 19:12:37 +0500

    done

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -1,13 +1,16 @@
 package lang.print.gaps.task3;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
-
         int a, b, c;
-        a = 10;
-        b = a;
-        c = b;
+        a = b = c = 10;
         System.out.println(c);
+
+        Logger logger= LoggerFactory.getLogger(DeclaringVars.class);
+        logger.info(String.valueOf(c));
+
     }
 }
